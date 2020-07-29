2ca3fa4f34cf5cbba7d439cef81541cb06884708
Kaishiyoku <dmp@nfze.de>
2020-07-29 22:00:11 +0200

    code tag adjustments

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,32 +13,32 @@
 
     <h2>Create entry</h2>
 
-    <pre><code>curl -d "content=YOUR CONTENT&expires=60" -H "Content-Type: application/x-www-form-urlencoded" -H "Accept: application/json" -X POST {{ route('entries.store') }}
+<pre><code>curl -d "content=YOUR CONTENT&expires=60" -H "Content-Type: application/x-www-form-urlencoded" -H "Accept: application/json" -X POST {{ route('entries.store') }}
     </code></pre>
 
-    <pre><code>fetch('{{ route('entries.store') }}', {
-        method: 'POST',
-        headers: {
-            'Content-Type' => 'application/x-www-form-urlencoded',
-            'Accept' => 'application/json',
-        },
-        body: {
-            content: 'YOUR CONTENT', // your text content
-            expires: 60, // expiration in minutes, defaults to {{ env('DEFAULT_EXPIRE_DURATION_IN_MINUTES') }} if field not set
-        },
-    })
-    </code></pre>
+<pre><code>fetch('{{ route('entries.store') }}', {
+    method: 'POST',
+    headers: {
+        'Content-Type' => 'application/x-www-form-urlencoded',
+        'Accept' => 'application/json',
+    },
+    body: {
+        content: 'YOUR CONTENT', // your text content
+        expires: 60, // expiration in minutes, defaults to {{ env('DEFAULT_EXPIRE_DURATION_IN_MINUTES') }} if field not set
+    },
+})
+</code></pre>
 
     <p>
         Returns:
     </p>
 
-    <pre><code class="php">{
-        "link": "{{ route('entries.show', ['uuid' => '<KEY_UUID>']) }}",
-        "delete_link": {{ route('entries.destroy', ['uuidDelete' => '<KEY_UUID_DELETE>']) }}",
-        "expires_at": "2020-07-29T17:19:18.045340Z"
-    }
-    </code></pre>
+<pre><code class="php">{
+    "link": "{{ route('entries.show', ['uuid' => '<KEY_UUID>']) }}",
+    "delete_link": {{ route('entries.destroy', ['uuidDelete' => '<KEY_UUID_DELETE>']) }}",
+    "expires_at": "2020-07-29T17:19:18.045340Z"
+}
+</code></pre>
 
     <p>The expires field is optional. A minimum of 5 and a maximum of 1440 minutes can be set. Default is {{ env('DEFAULT_EXPIRE_DURATION_IN_MINUTES') }} minutes until the entry expires.</p>
 
