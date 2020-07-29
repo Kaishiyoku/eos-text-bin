c51d95e691e55da0cd4f9706eefff0df36fd082d
Kaishiyoku <dmp@nfze.de>
2020-07-29 18:14:36 +0200

    added scheduler for cleaning up expired entries

---
+++ app/Console/Commands/CleanUpExpiredEntries.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Entry;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class CleanUpExpiredEntries extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'entries:cleanup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clean up expired entries';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $expiredEntries = Entry::where('expires_at', '<', Carbon::now())->delete();
+
+        $this->line('Expired entries deleted.');
+
+        return 0;
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\CleanUpExpiredEntries;
 use Illuminate\Console\Scheduling\Schedule;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
 
@@ -14,6 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         \Illuminate\Console\KeyGenerateCommand::class,
+        CleanUpExpiredEntries::class,
     ];
 
     /**
@@ -24,6 +26,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        //
+        $schedule->command('entries:cleanup')->hourlyAt(0);
     }
 }
